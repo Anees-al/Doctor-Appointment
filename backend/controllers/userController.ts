ef06da43f6cc7ddd258a133ad5142facd3897ec7
Anees A L <aneesal874@gmail.com>
2026-01-21 12:25:58 +0530

    small fixes

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -38,12 +38,12 @@ export const createUser=async(req:Request,res:Response)=>{
     {expiresIn:'1d'})
 
 
-    res.cookie('token',token,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV === 'production',
-        sameSite:process.env.NODE_ENV === 'production' ?'none':'strict',
-        maxAge:7*24*60*60*1000
-    })
+    res.cookie('token', token, {
+    httpOnly: true,
+    secure: true,              
+    sameSite: 'none',         
+    maxAge: 7*24*60*60*1000
+})
 
 
     await newUser.save();
@@ -82,12 +82,12 @@ export const login=async(req:Request,res:Response)=>{
     {expiresIn:'1d'})
 
 
-    res.cookie('token',token,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV === 'production',
-        sameSite:process.env.NODE_ENV === 'production' ?'none':'lax',
-        maxAge:7*24*60*60*1000
-    })
+    res.cookie('token', token, {
+    httpOnly: true,
+    secure: true,           
+    sameSite: 'none',       
+    maxAge: 7*24*60*60*1000
+})
             return res.status(200).json({message:'successfully loged in',existingUser,success:true});
            } catch (error:any) {
             return res.status(400).json({message:error.message})
