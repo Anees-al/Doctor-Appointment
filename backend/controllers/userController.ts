ee30d66a54b8f178e6a6941b816b34dcea0a57cd
Anees A L <aneesal874@gmail.com>
2026-01-21 11:59:36 +0530

    small fixes

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -85,10 +85,10 @@ export const login=async(req:Request,res:Response)=>{
     res.cookie('token',token,{
         httpOnly:true,
         secure:process.env.NODE_ENV === 'production',
-        sameSite:process.env.NODE_ENV === 'production' ?'none':'strict',
+        sameSite:process.env.NODE_ENV === 'production' ?'none':'lax',
         maxAge:7*24*60*60*1000
     })
-            return res.status(200).json({message:'successfully loged in',existingUser,token,success:true});
+            return res.status(200).json({message:'successfully loged in',existingUser,success:true});
            } catch (error:any) {
             return res.status(400).json({message:error.message})
            }
